39c6ee31e7f797cc677d6fdadcefed6f1a817aff
Royalenfield350 <69975709+Royalenfield350@users.noreply.github.com>
2020-10-15 11:59:25 +0530

    Add files via upload

---
+++ day3.java
@@ -0,0 +1,48 @@
+class Test
+{
+public static void main(String args[])
+{
+     float eng, phy, chem, math, comp; 
+     double total, average, percentage;
+    Scanner op=new Scanner(System.in);
+    /* Input marks of all five subjects */
+    System.out.println("Enter marks of five subjects out of 100 \n");
+    System.out.print("Enter marks of English subjects:");
+    eng=op.nextFloat();
+    System.out.print("Enter marks of physics subjects:");
+    phy=op.nextFloat();
+    System.out.print("Enter marks of chemistry subjects:");
+    chem=op.nextFloat();
+    System.out.print("Enter marks of maths subjects:");
+    math=op.nextFloat();
+    System.out.print("Enter marks of computers subjects:");
+    comp=op.nextFloat();
+
+    /* Calculate total, average and percentage */
+    total = eng + phy + chem + math + comp;
+    average = (total / 5.0);
+    percentage = (total / 500.0) * 100;
+
+    /* Print all results */
+    System.out.println("Total marks ="+total);
+    System.out.println("Average marks = "+average);
+    System.out.println("Percentage = "+percentage);
+
+    System.out.print("The student Grade is: ");
+        if(percentage>=80)
+        {
+            System.out.print("A");
+        }
+        else if(percentage>=60 && percentage<80)
+        {
+           System.out.print("B");
+        } 
+        else if(percentage>=40 && percentage<60)
+        {
+            System.out.print("C");
+        }
+        else
+        {
+            System.out.print("D");
+        }	
+    }
